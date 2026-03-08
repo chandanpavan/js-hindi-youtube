9ebe36956d6bbb10c2bd63dca637da24cf61ee59
Chandan <chandu.h2024@gmail.com>
2026-03-08 18:19:32 +0530

    oops-started

--- test.js
+++ test.js
@@ -1,106 +1,12 @@
-const promiseOne = new Promise(function (resolve, reject) {
-  // Do an async task
-  // DB calls, cryptography, network call
-  setTimeout(function () {
-    console.log("Async task is complete");
-    resolve(); // the connection between the promise and then()
-  }, 1000);
-});
+let myHeros = ["thor", "spiderman"];
 
-promiseOne.then(function () {
-  console.log("Promise consumed");
-});
+let heroPower = {
+  thor: "hammer",
+  spiderman: "sling",
 
-new Promise(function (resolve, reject) {
-  setTimeout(function () {
-    console.log("Async task 2");
-    resolve();
-  }, 1000);
-}).then(function () {
-  console.log("Async 2 resolved");
-});
+  getSpiderPower: function () {
+    console.log(`Spidy power is ${this.spiderman}`);
+  },
+};
 
-const promiseThree = new Promise(function (resolve, reject) {
-  setTimeout(function () {
-    resolve({ username: "Chai", email: "example@email.com" });
-  }, 1000);
-});
-
-promiseThree.then(function (user) {
-  console.log(user);
-});
-
-const promiseFour = new Promise(function (resolve, reject) {
-  setTimeout(function () {
-    let error = true;
-    if (!error) {
-      resolve({
-        username: "hitesh",
-        password: "123",
-      });
-    } else {
-      reject("ERROR: Something went wrong");
-    }
-  }, 1000);
-});
-
-promiseFour
-  .then(function (user) {
-    console.log(user);
-    return user.username;
-  })
-  .then((username) => {
-    console.log(username);
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
-  .finally(() => {
-    console.log("The promise is either resolved or rejected");
-  });
-
-const promiseFive = new Promise((resolve, reject) => {
-  setTimeout(function () {
-    let error = true;
-    if (!error) {
-      resolve({
-        username: "javascript",
-        email: "example@email.com",
-      });
-    } else {
-      reject("JS went Wrong");
-    }
-  }, 1000);
-});
-
-async function consumePromiseFive() {
-  try {
-    const response = await promiseFive;
-    console.log(response);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-consumePromiseFive();
-
-// async function getAllUser() {
-//   const response = await fetch("https://api.github.com/users/chandanpavan");
-//   const data = await response.json();
-//   console.log(data);
-// }
-
-// getAllUser();
-
-fetch("https://api.github.com/users/chandanpavan")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-
-// fetch
+// heroPower.
